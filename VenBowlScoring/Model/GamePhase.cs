85b14f531b1dacb29a0c61a3d66d75bda5ce2f46
Klynt Gerde <gerdeke@gmail.com>
2018-10-20 20:01:39 -0400

    Game Phases ready for review.

--- VenBowlScoring/Model/GamePhase.cs
+++ VenBowlScoring/Model/GamePhase.cs
@@ -1,9 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace VenBowlScoring.Model
 {
+    /// <summary>
+    /// This object is used by a game to provide players time to join the game/play/review and celebrate the game/clean up after the game.
+    /// </summary>
     public class GamePhase
     {
         /// <summary>
@@ -21,12 +22,19 @@ namespace VenBowlScoring.Model
         ///         Once I have configurations working I would move this to a configuration file. but for now this will be fine.
         ///     </TODO>
         /// </summary>
-        public static List<string> Phases = new List<string> {"Setup","Play","Review","Cleanup"};
+        public static List<string> Phases = new List<string> {"Setup","Play","Review","Clean up"};
 
+        /// <summary>
+        /// Initialize the Game Phases and set the initial phase as the current phase.
+        /// </summary>
         GamePhase ()
         {
             CurrentPhase = Phases[0];
         }
+
+        /// <summary>
+        /// change the phase to the next phase.
+        /// </summary>
         void ChangePhase()
         {
             CurrentPhase = Phases[Phases.IndexOf(CurrentPhase) + 1];
